f7d4b6688a977d9ca7a8720b3f719f09244430e4
Ezequiel Dhonatan <Ezequieldhonatan@gmail.com>
2020-02-16 17:55:47 -0300

    Module 2.1 - Contact (Updated)

--- resources/js/components/api/v1/system/panel/support/contact/Contact.vue
+++ resources/js/components/api/v1/system/panel/support/contact/Contact.vue
@@ -48,7 +48,7 @@
 
                             <div class="col-sm-12 col-xs-12 col-lg-12 col-md-12">
 
-                                <!-- TABLE (MODULE 2.1)
+                                <!-- TABLE
                                 ================================================== -->
                                 <div class="table-responsive">
 
